7849c4728199c4241579b00eb9bf7ceefebd4622
Harini M <111434270+HariniAug08@users.noreply.github.com>
2023-05-18 10:43:23 +0530

    Add files via upload

---
+++ Train.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Train {
+
+	public static void main(String[] args) {
+		int trainNo=25489;
+		
+		boolean isOccupied=true;
+		String trainName="Train name Ulavan";
+		
+		System.out.println(trainNo+" "+trainName);
+		System.out.println(isOccupied);
+	}
+
+}
