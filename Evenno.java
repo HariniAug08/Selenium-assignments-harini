98005db4a998602c6d431ce2d2a1fa7b745896b2
Harini M <111434270+HariniAug08@users.noreply.github.com>
2023-05-18 12:38:19 +0530

    Add files via upload

---
+++ Evenno.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class Evenno {
+
+	public static void main(String[] args) {
+		int n=20;
+		for (int i = 0; i <=n; i ++)
+		
+			if(i%2!=1) {
+		System.out.println("even number :"+i);
+			}
+		
+	}				
+}
---
+++ oddno.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class oddno {
+	public static void main(String[] args) {
+		int n=37;
+		for( int i=1;i<=n;i++)
+			if(i%2!=0) {
+		System.out.println("odd number :"+i);
+			}
+		
+	}	
+	}
+
